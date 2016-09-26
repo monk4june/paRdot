2a53ec9aeaee55ffbdaf0936f2565a3a6cdcce8b
Stuart Illson <sillson88@gmail.com>
2016-09-26 14:31:32 -0700

    will return full df

--- R/pardot_client.R
+++ R/pardot_client.R
@@ -52,30 +52,32 @@ pardot_client.api_call <- function(request_url) {
     resp <- GET(request_url, content_type_json())
     jsonresp <- fromJSON(request_url)
   }
-  
+
   raw_df <- pardot_client.get_data_frame(request_url)
+  raw_df$result.prospect.campaign_name <- raw_df$result.prospect.campaign$name
+  raw_df <- subset(raw_df, select=-result.prospect.campaign)
   lowest_date <- tail(raw_df, 1)$result.prospect.created_at
   polished_df <- rbind(raw_df)
-  sink(polished_df)
-  
+
   while (!nrow(raw_df) < 200) {
-    sink("we made it to the loop")
+    print(paste0("Pulling data from", lowest_date))
     loop_url <- pardot_client.iterative_request_url(request_url, lowest_date)
     raw_df <- pardot_client.get_data_frame(loop_url)
-    lowest_date <- raw_df[200,]$result.prospect.created_at
+    lowest_date <- tail(raw_df, 1)$result.prospect.created_at
+    raw_df$result.prospect.campaign_name <- raw_df$result.prospect.campaign$name
+    raw_df <- subset(raw_df, select=-result.prospect.campaign)
     polished_df <- rbind(polished_df, raw_df)
   }
-  
-  polished_df$result.prospect.campaign_name <- polished_df$result.prospect.campaign$name
-  polished_df <- subset(polished_df, select=-result.prospect.campaign)
-  
-  
+
+
+
 
   #xml_response <- xmlNode(content(resp, "parsed"))
   return(polished_df)
 }
 
 pardot_client.iterative_request_url <- function(requestUrl, theDate) {
+  theDate <- gsub(' ', 'T', theDate)
   iterative_request_url <- paste0(requestUrl,"&created_after=",theDate,"&sort_by=created_at&sort_order=ascending")
 }
 

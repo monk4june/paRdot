d49ecf70f0e00c9437e69f312cfac25da2887fb8
demgenman <demgenman@outlook.com>
2017-11-16 23:57:34 +0100

    Make API calls with selection parameters, results in JSON. Naviagtion uses offset. Returns data frame. Closes #1, closes #2, closes #4, closes #5, closes #6.

--- R/pardot_client.R
+++ R/pardot_client.R
@@ -1,21 +1,26 @@
-#' Make a call to the Pardot API and return XML
+#' Make a call to the Pardot API and return XML or data frame
 #'
 #' @param object A string containing a Pardot Object
 #' @param operator A string containing a Pardot Operator
 #' @param identifier_field A string with an optional identifier field. Can be null
 #' @param identifier A string with an optional identifier that can be null if identifier_field is null
+#' @param request_pars A string of query parameters. Can be null
+#' @param result_format A string specifying the result format used for API calls: "json" or "xml". If json, pardot_client() returns a data frame.
+#' @return XML or a data frame in tbl_df (dplyr) format
 #' @examples
 #' \dontrun{
 #' set_credentials("your-username", "your-password", "your-user-key")
-#' pardot_client("campaign", "query")}
-#' @export
-#' pardot_client
+#' pardot_client("campaign", "query")
+#' pardot_client(object = "campaign", operator = "query", 
+#'   request_params = "created_after=yesterday&id_greater_than=1234XYZ")}
+#' @export pardot_client
 #' @import httr
 #' @import xml2
 #' @import XML
 #' @import jsonlite
+#' @import dplyr
 
-pardot_client <- function(object,operator,identifier_field=NULL,identifier=NULL) {
+pardot_client <- function(object, operator, identifier_field=NULL, identifier=NULL, request_pars=NULL, result_format="json") {
   # object & operator are required fields
   # identifier fields / identifier are optional
   # optional field to implement <- api_request_params,"&format=",api_format
@@ -26,11 +31,15 @@ pardot_client <- function(object,operator,identifier_field=NULL,identifier=NULL)
   } else if (exists('api_key') && api_key == "Login failed" ) {
     pardot_client.authenticate()
   } else {
+	message("Perform API request")
     request_url <- pardot_client.build_url(param_list)
-    pardot_client.api_call(request_url)
+	if (result_format == "json") {
+		pardot_client.api_call_json(request_url)
+	} else {
+		pardot_client.api_call(request_url)
+	}
   }
 }
-
 
 pardot_client.authenticate <- function() {
   # body params must be set in list. Add .env get that will fetch these items
@@ -43,6 +52,44 @@ pardot_client.authenticate <- function() {
 
   # returns xml node with <api_key>
   api_key <<- xml_text(content(fetch_api_call))
+}
+
+pardot_client.api_call_json <- function(request_url) {
+	
+	# Perform request
+	resp <- GET(request_url)
+	if (resp$status != 200) {
+		pardot_client.authenticate()
+		resp <- GET(request_url, content_type_json())
+		jsonresp <- fromJSON(request_url)
+	}
+
+	# Retrieve results in chunks
+	polished_df <- data.frame()
+	ready <- FALSE
+	n_offset <- 0
+	while (!ready) {
+		cat(".")
+		if (n_offset == 0) {
+			raw_df <- pardot_client.get_data_frame(request_url)
+		} else {
+			raw_df <- pardot_client.get_data_frame(pardot_client.iterative_request_url(request_url, n_offset = n_offset))
+		}
+		# Substitute embedded campaign dataframe column by the "name" column it contains
+		colCampaign <- grep("\\.campaign$", colnames(raw_df), ignore.case = FALSE, value = TRUE)
+		colCampaignName <- paste0(colCampaign, "_name")
+		raw_df[, colCampaignName] <- raw_df[, colCampaign]$name
+		raw_df[, colCampaign] <- NULL
+		# Append
+		n <- nrow(raw_df)
+		if (n > 0) {
+			n_offset <- n_offset + n
+			polished_df <- bind_rows(polished_df, raw_df)
+		} else {
+			ready <- TRUE
+		}
+	}
+	return(polished_df)
 }
 
 pardot_client.api_call <- function(request_url) {
@@ -72,19 +119,33 @@ pardot_client.build_url <- function(param_list) {
   # optional fields
   api_identifier_field = pardot_client.scrub_opts(param_list$identifier_field)
   api_identifier = pardot_client.scrub_opts(param_list$identifier)
+  api_request_params = param_list$request_pars
+  api_request_params <- if (!is.null(api_request_params)) paste0("&", sub("^&+", "", api_request_params)) else NULL  
 
-  request_url <- paste0("https://pi.pardot.com/api/",api_object,"/version/3/do/",api_operator,api_identifier_field,api_identifier,"?api_key=",api_key,"&user_key=",Sys.getenv("PARDOT_USER_KEY"),"&output=bulk&format=json")
+  request_url <- paste0("https://pi.pardot.com/api/",api_object,"/version/3/do/",api_operator,api_identifier_field,api_identifier,"?api_key=",api_key,"&user_key=",Sys.getenv("PARDOT_USER_KEY"),api_request_params,"&output=bulk&format=json")
+  print(request_url)
   return(request_url)
 }
 
-pardot_client.iterative_request_url <- function(requestUrl, theDate) {
-  theDate <- gsub(' ', 'T', theDate)
-  iterative_request_url <- paste0(requestUrl,"&created_after=",theDate,"&sort_by=created_at&sort_order=ascending")
+pardot_client.iterative_request_url <- function(request_url, theDate = NULL, n_offset = NULL) {
+	# Keep original parameter theDate for backward compatibility
+	# Check n_offset first as offset is the preferred method for navigating.
+	# Consequence is that n_offset must be named when the function is called.
+	if (!missing(n_offset)) {
+		iterative_request_url <- paste0(request_url, "&offset=", n_offset)
+	} else if (!missing(theDate)) {
+		theDate <- gsub(' ', 'T', theDate)
+		iterative_request_url <- paste0(requestUrl,"&created_after=",theDate,"&sort_by=created_at&sort_order=ascending")
+	} else {
+		iterative_request_url <- request_url
+	}
+	print(iterative_request_url)
+	return(iterative_request_url)
 }
 
 pardot_client.scrub_opts <- function(opt) {
   if( is.null(opt) || opt == '' ) {
-    return('/')
+    return('')
   } else {
     new_opt <- paste0('/',opt)
     return(new_opt)
